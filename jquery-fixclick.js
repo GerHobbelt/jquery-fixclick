ae47bdaff8ad343e99036c4a11080aa1be1d3505
Ger Hobbelt <ger@hobbelt.com>
2014-01-20 09:03:34 +0100

    invoke the handlers with proper `this` context

--- jquery-fixclick.js
+++ jquery-fixclick.js
@@ -11,28 +11,29 @@
  */
 if (jQuery) {
     (function ($) {
-        $.fn.fixClick = function (B, A) {
+        $.fn.fixClick = function (click_handler, dblclick_handler) {
             var _ = this;
-            this.click = B;
-            this.dblclick = A;
-            this.firstClick = false;
-            this.timer = null;
-            this.delay = 300;
+            _.click = click_handler;
+            _.dblclick = dblclick_handler;
+            _.firstClick = false;
+            _.timer = null;
+            _.delay = 300;
             $(this)
-                .click(function ($) {
-                    var A = this;
-                    this.e = $;
-                    _.firstClick == false ? _.timer = setTimeout(function () {
-                        _.click(A.e);
-                        _.firstClick = false;
-                    }, _.delay) : "";
-                    _.firstClick = true;
-                })
-                .dblclick(function ($) {
-                    clearTimeout(_.timer);
+            .click(function (e) {
+                var _self = this;
+                _self.e = e;
+                _.firstClick == false ? _.timer = setTimeout(function () {
+                    _.click.call(_self, _self.e);
                     _.firstClick = false;
-                    _.dblclick($);
-                });
+                }, _.delay) : null;
+                _.firstClick = true;
+            })
+            .dblclick(function (e) {
+                var _self = this;
+                clearTimeout(_.timer);
+                _.firstClick = false;
+                _.dblclick.call(_self, e);
+            });
             return this;
         }
     })(jQuery);
