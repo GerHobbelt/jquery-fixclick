5a6493a1a622a16f2e353d1d844edfaa9806626b
Ger Hobbelt <ger@hobbelt.com>
2014-01-20 08:56:35 +0100

    format code

--- jquery-fixclick.js
+++ jquery-fixclick.js
@@ -9,29 +9,31 @@
  *   http://www.opensource.org/licenses/mit-license.php
  *
  */
-if (jQuery)(function ($) {
-    $.fn.fixClick = function (B, A) {
-        var _ = this;
-        this.click = B;
-        this.dblclick = A;
-        this.firstClick = false;
-        this.timer = null;
-        this.delay = 300;
-        $(this)
-            .click(function ($) {
-                var A = this;
-                this.e = $;
-                _.firstClick == false ? _.timer = setTimeout(function () {
-                    _.click(A.e);
-                    _.firstClick = false
-                }, _.delay) : "";
-                _.firstClick = true
-            })
-            .dblclick(function ($) {
-                clearTimeout(_.timer);
-                _.firstClick = false;
-                _.dblclick($)
-            });
-        return this
-    }
-})(jQuery)
+if (jQuery) {
+    (function ($) {
+        $.fn.fixClick = function (B, A) {
+            var _ = this;
+            this.click = B;
+            this.dblclick = A;
+            this.firstClick = false;
+            this.timer = null;
+            this.delay = 300;
+            $(this)
+                .click(function ($) {
+                    var A = this;
+                    this.e = $;
+                    _.firstClick == false ? _.timer = setTimeout(function () {
+                        _.click(A.e);
+                        _.firstClick = false;
+                    }, _.delay) : "";
+                    _.firstClick = true;
+                })
+                .dblclick(function ($) {
+                    clearTimeout(_.timer);
+                    _.firstClick = false;
+                    _.dblclick($);
+                });
+            return this;
+        }
+    })(jQuery);
+}
